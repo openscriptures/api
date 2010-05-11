53c891c9fa633c83149f8e110688669b8144324f
Weston Ruter <westonruter@gmail.com>
2010-05-11 14:12:07 -0700

    License block

--- osis.py
+++ osis.py
@@ -1,9 +1,32 @@
-# Copyright (C) 2010 OpenScriptures.org
-# Open source dual-licensed:
-#  - MIT: http://creativecommons.org/licenses/MIT/
-#  - GPL: http://creativecommons.org/licenses/GPL/2.0/
+# -*- coding: utf8 -*-
+# 
+# osis.py: Module for repesenting OSIS objects (osisIDs, osisRefs, etc)
+# Latest: http://github.com/openscriptures/api/blob/master/osis.py
+# 
+# Copyright (C) 2010 OpenScriptures.org contributors
+# Dual licensed under the MIT or GPL Version 2 licenses.
+# 
+# MIT License (appears below): http://creativecommons.org/licenses/MIT/
+# GPL license: http://creativecommons.org/licenses/GPL/2.0/
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
-# coding: utf8 #
 import re
 from datetime import date, time, datetime
 
