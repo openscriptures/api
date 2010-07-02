b73b8a2574bde9b90b53fe030e3120b80cd95faf
Weston Ruter <westonruter@gmail.com>
2010-07-01 22:59:15 -0700

    Cleaning up license info

--- osis.py
+++ osis.py
@@ -3,36 +3,16 @@
 Module for repesenting OSIS objects (osisIDs, osisRefs, etc)
 
 Latest: http://github.com/openscriptures/api/blob/master/osis.py
-Copyright (C) 2010 Open Scriptures <openscriptures.org> contributors
+Copyright (C) 2010 Open Scriptures <openscriptures.org> contributors (see credits below)
 Dual licensed under the MIT or GPL Version 2 licenses.
 MIT License: http://creativecommons.org/licenses/MIT/
 GPL 2.0 license: http://creativecommons.org/licenses/GPL/2.0/
 
-= CHANGELOG =
-== 1.1.1 (2010-06-30) ==
- - Using `publish_date` and `publish_date_granularity` instead of `pub_date*`
+CHANGELOG
+ - 1.1.1 (2010-06-30)
+   * Using `publish_date` and `publish_date_granularity` instead of `pub_date*`
  
 """
-
-# MIT License:
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 __credits__ = [
     "Weston Ruter",
     # join me!
