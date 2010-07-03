f3d69056719fc0132a888895dc45cd86d0acd50d
Patrick Altman <paltman@gmail.com>
2010-07-03 01:40:23 -0500

    correct old app name

--- openscriptures_texts/urls.py
+++ openscriptures_texts/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.defaults import patterns
 # osis module can define a parser, osisRef object
 
 urlpatterns = patterns('',
-    (r'^rest/passage/(?P<osis_ref>.+)$', 'api.rest.views.passage'),
+    (r'^rest/passage/(?P<osis_ref>.+)$', 'openscriptures_texts.rest.views.passage'),
 )
 
 
